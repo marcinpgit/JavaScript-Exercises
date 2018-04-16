848956c4acaeb7f07ef4bdef170cbe942ae01b87
Marcin Płużyński <marcinp2012@gmail.com>
2018-04-16 21:02:51 +0200

    arr exer

--- Array/script_4.js
+++ Array/script_4.js
@@ -41,3 +41,33 @@ console.log(binarySearch(items, 1));
 console.log(binarySearch(items, 5));
 
 
+// There are two arrays with individual values, write a JavaScript program to compute the sum of each individual index value from the given arrays.
+// Sample array :
+//     array1 = [1,0,2,3,4];
+// array2 = [3,5,6,7,8,13];
+// Expected Output :
+//     [4, 5, 8, 10, 12, 13]
+
+console.log('script 3');
+
+var array1 = [1, 0, 2, 3, 4];
+var array2 = [3, 5, 6, 7, 8, 13];
+var sum = [];
+var counter = 0;
+
+while(counter < array1.length && counter < array2.length) {
+    sum.push(array1[counter] + array2[counter]);
+    counter++;
+}
+
+if(counter === array1.length) {
+    for(var k = counter; k < array2.length; k++) {
+        sum.push(array2[k]);
+    }
+} else {
+    for(var j = counter; j < array1.length; j++) {
+        sum.push(array1[j]);
+    }
+}
+
+console.log(sum);
